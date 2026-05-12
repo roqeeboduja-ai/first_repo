acb878e5b1c0e19e76e6fb425743d275a8b8d9e1
Roqeeb <odujaroqeeb@gmail.com>
2026-05-12 21:00:01 +0100

    Switch Case

---
+++ switchCase.js
@@ -0,0 +1,74 @@
+// let day = "Sunday";
+// switch (day){
+//     case "Monday":
+//         console.log("It is monday!")
+//         break;
+
+//     case "Tuesday":
+//         console.log("It is a Tuesday!");
+//         break;
+
+//     case "Saturday":
+//     case "Sunday":
+//         break;
+
+//     default:
+//         console.log("It's just a normal day!.")
+// }
+
+
+
+// let coffeeSize = "medium";
+
+// switch (coffeeSize){
+//     case "small":
+//         console.log("That will be $2.00.");
+//         break;
+//     case "medium":
+//         console.log("That will be $3.00");
+//         break;
+//     case "large":
+//         console.log("That will be $4.00");
+//         break;
+//     default:
+//         console.log("Sorry, we don't have that size.");
+// }
+
+
+
+// let day = "Saturday";
+
+// switch (day){
+//     case "Monday":
+//     case "Tuesday":
+//     case "Wednesday":
+//     case "Thursday":
+//     case "Friday":
+//         console.log("It's weekday, Always Stressful.")
+//         break;
+
+//     case "Saturday":
+//     case "Sunday":
+//         console.log("It's weekend. I love it!.");
+//         break;
+
+//     default:
+//         console.log("That's not a real day.");
+// }
+
+
+let signal = "red";
+
+switch (signal){
+    case "green":
+        console.log("Accelerate!.");
+        break;
+    case "yellow":
+        console.log("Prepare to stop!.");
+        break;
+    case "red":
+        console.log("Stop immediately!.");
+        break;
+    default:
+        console.log("Signal malfunctioning.")
+}
